1c9e07ef8d09f173d4a636990023cc16b18e368e
Flávia Ferrari <flaviaferrarit@gmail.com>
2022-08-05 22:01:28 -0300

    movie list

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,6 +2,9 @@
 
 import { useEffect, useState } from 'react';
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
+import "./syled.css"
+
 
 function Home() {
     const [filmes, setFilmes] = useState([]);
@@ -15,7 +18,8 @@ function Home() {
                     page: 1,
                 }
             })
-            console.log(response.data.results);
+            // console.log(response.data.results.slice(0,10));
+            setFilmes(response.data.results.slice(0, 10))
         }
 
         loadFilmes();
@@ -23,8 +27,18 @@ function Home() {
     }, []);
 
     return (
-        <div>
-            <h1>Bem Vindo a Home</h1>
+        <div className='cantainer'>
+            <div className='lista-filmes'>
+                {filmes.map((filme) => {
+                    return (
+                        <article key={filme.id}>
+                            <strong>{filme.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+                        <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                        </article>
+                    )
+                })}
+            </div>
         </div>
     )
 }
