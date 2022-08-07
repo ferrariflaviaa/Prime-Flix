124696a12c8053bf138b05109e37dcb1bd000e70
Flávia Ferrari <flaviaferrarit@gmail.com>
2022-08-06 21:26:20 -0300

    creating bookmarks page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,10 @@
 import "./syled.css"
-import {Link} from 'react-router-dom'
-function Header (){
-    return(
+import { Link } from 'react-router-dom'
+function Header() {
+    return (
         <header>
             <Link className="logo" to="/">Prime Flix</Link>
-            <Link className="favoritos" to="/favorite">Meus filmes favoritos</Link>
+            <Link className="favoritos" to="/favoritos">Meus filmes</Link>
         </header>
     )
 }
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Filme from "./pages/Filme";
 import Header from "./components/Header";
+import Favoritos from "./pages/favoritos";
 import Erro from "./pages/Erro";
 
 function RoutesApp() {
@@ -12,6 +13,7 @@ function RoutesApp() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/filme/:id" element={<Filme />} />
+                <Route path="/favoritos" element={<Favoritos />} />
                 <Route path="*" element={<Erro />} />
             </Routes>
         </BrowserRouter>
